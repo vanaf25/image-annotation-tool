15f13271dccdf53c0c432abc706225b60e0c9244
Vanay <vanayfefilov777@gmail.com>
2022-11-09 23:24:47 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {defaultShapeStyle, ReactPictureAnnotation} from "react-picture-annotation";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pageSize, setPageSize] = useState({
+        width:0,
+        height:0
+    });
+    const onResize = () => {
+        setPageSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", onResize);
+        onResize();
+        return () => window.removeEventListener("resize", onResize);
+    }, []);
+    const onSelect = (selectedId:any) => console.log(selectedId);
+    const onChange = (data:any) => console.log("data inform", data);
+
+    return (
+        <div className="App">
+            <ReactPictureAnnotation
+                image="https://source.unsplash.com/random/800x600"
+                onSelect={onSelect}
+                onChange={onChange}
+                width={pageSize.width}
+                height={pageSize.height}
+                scrollSpeed={0}
+            />
+        </div>
+    );
 }
 
 export default App;
